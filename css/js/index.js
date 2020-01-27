689e06ec8d20c23184a8f08173c6c35df6d9bb4d
eliza <samoylovaeliza@gmail.com>
2020-01-27 12:20:14 +0300

    modified index.js

--- css/js/index.js
+++
@@ -1,9 +0,0 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
-// var myURL = 'https://app.fakejson.com/q/6NyONp5Y';
-// var token = 'Gfce9JoM4L6XSxiifVT_jw';
-xhr.open(GET, 'https://app.fakejson.com/q/6NyONp5Y?token=Gfce9JoM4L6XSxiifVT_jw');
-xhr.send();
-xhr.onload = function() {
-    console.log(this);
-  };
--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
 var myURL = 'https://app.fakejson.com/q/6NyONp5Y';
 var token = 'Gfce9JoM4L6XSxiifVT_jw';
-xhr.open(GET, `${myURL}?token=${token}`);
+xhr.open('GET', `${myURL}?token=${token}`);
 xhr.send();
 xhr.onload = function() {
-    console.log(this);
+  if (xhr.status != 200) { 
+    alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); 
+  } else {
+    // console.log(`JSON: ${xhr.responseText}`);
+    var myJson = JSON.parse(xhr.responseText)["goods"];
+    console.log(myJson);
+  }
   };
+
+console.log('myJson');
+
+// xhr = JSON.parse(xhr.responseText);
